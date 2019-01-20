cb4c844d6590160d829521afed1d466d717450db
Nilesh Jayanandana <nilesh.jayanandana@yahoo.com>
2019-01-20 15:21:38 +0530

    bug fix

--- index.ts
+++ index.ts
@@ -6,7 +6,7 @@ var app = express();
 app.get('/', (req, res) => {
     res.send(`Hello World 123 with cicd hellos. ${process.env.type}`)
 })
-app.listen1(8000, () => {
+app.listen(8000, () => {
     console.log("app works on http://localhost:8000");
 });
 
