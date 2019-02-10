df0abb309f1e5c1bd88ccfe6b0613c72d28331b5
Nilesh Jayanandana <nilesh.jayanandana@yahoo.com>
2019-02-10 17:01:04 +0530

    minor change

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ var app = express();
 
 
 app.get('/', (req, res) => {
-    res.send(`Hello World  running a new version. ${process.env.type}`)
+    res.send(`Hello World  running 123. ${process.env.type}`)
 })
 app.listen(8000, () => {
     console.log("app works on http://localhost:8000");
